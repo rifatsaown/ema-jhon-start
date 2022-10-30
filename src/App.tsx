1c1eb16b3bc2ea4f159c87cdf7aab280d3bd033b
rifatsaown <rifatsaown0@gmail.com>
2022-10-31 00:53:32 +0600

    initial commit again

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <p>This is for test the app</p>
-      <Header />
     </div>
   );
 }
