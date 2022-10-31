2b35a7fe1df4754ef3a0edb9498613166f6e132a
rifatsaown <rifatsaown0@gmail.com>
2022-10-31 18:05:48 +0600

    header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <p>This is for test the app</p>
+      <Header />
     </div>
   );
 }
